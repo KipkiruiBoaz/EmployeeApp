ec738c06ea06f35d3a387f9717d55d03c3bee8ef
Gilbert kibet <kibetgilly354@gmail.com>
2023-07-31 20:29:13 +0300

    code review and refactoring

--- WebApplication1/Data/EmployeeDbContext.cs
+++ WebApplication1/Data/EmployeeDbContext.cs
@@ -5,9 +5,24 @@ namespace WebApplication1.Data
 {
     public class EmployeeDbContext : DbContext
     {
-        public EmployeeDbContext(DbContextOptions options) : base(options)
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
         {
         }
         public DbSet<Employees> Employees { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.HasIndex(i => i.Name).IsUnique();
+
+                /// required
+                
+                entity.Property(p=>p.Department).IsRequired();
+
+            }
+            );
+        }
     }
 }
